2d650f9d86331c70fb399a451820f986a5df16bd
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-19 14:56:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ let sonuc2=document.getElementById("sonuc2");
 let gir=document.getElementById("btnGir");
 let rastgele1=Math.floor(Math.random()*20);
 let rastgele2=Math.floor(Math.random()*20);
+let yeniSayi1=rastgele1;
+let yeniSayi2=rastgele2;
 
 gir.addEventListener("click", ()=>{
     girilenSayi1.innerHTML=sayi1;
     console.log(sayi1);
-    sonuc1.innerHTML=rastgele1;
+    sayi1=yeniSayi1;
+    sonuc1.innerHTML=sayi1;
     console.log(rastgele1);
 
     girilenSayi2.innerHTML=sayi2;
     console.log(sayi2);
-    sonuc2.innerHTML=rastgele2;
+    sayi2=yeniSayi2;
+    sonuc2.innerHTML=sayi2;
     console.log(rastgele2);
-})
+});
